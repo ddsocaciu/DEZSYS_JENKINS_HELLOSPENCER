827b002d4f63277ec0c6ce62d2839bf1ed17a564
dsocaciu <dsocaciu@studen.tgm.ac.at>
2026-05-12 11:36:37 +0200

    minor change

--- src/hello.py
+++ src/hello.py
@@ -22,5 +22,6 @@ def hello_spencer():
 
 # Run the application
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5556)
+    #app.run(debug=True, host='0.0.0.0', port=5556)
+    app.run(host='0.0.0.0', port=5556)
 # Änderung
